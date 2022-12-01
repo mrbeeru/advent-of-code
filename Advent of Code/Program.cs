38f9839f7859f59e774216cb879d515f8f0faf72
mrbeeru <andrei_bublea@yahoo.com>
2022-12-01 23:47:07 +0200

    small refactor - moved resolver code into its own class

--- Advent of Code/Program.cs
+++ Advent of Code/Program.cs
@@ -10,24 +10,8 @@ Parser.Default.ParseArguments<Options>(args).WithParsed((opt) => Invoke(opt));
 
 void Invoke(Options opt)
 {
-    if (opt.Part != 1 && opt.Part != 2)
-        throw new Exception("Part can be 1 or 2");
-
-    var cls = Assembly.GetExecutingAssembly()
-        .GetTypes()
-        .Single(x => x.FullName == $"AdventOfCode.Y{opt.Year}.Day{opt.Day:00}");
-
-    if (cls == null)
-        throw new Exception($"AOC {opt.Year}-{opt.Day:00} is not implemented.");
-
-    var method = cls.GetMethod($"Part{opt.Part}");
-
-    if (method == null)
-        throw new Exception($"AOC {opt.Year}-{opt.Day:00} Part {opt.Part} is not implemented.");
-
     var fileInputReader = new FileInputReader(opt.FilePath);
-    
-    var instance = Activator.CreateInstance(cls, new object[] { fileInputReader });
+    var (instance, method) = Resolver.Resolve(opt.Year, opt.Day, opt.Part, fileInputReader);
     var result = method.Invoke(instance, null);
 
     Console.WriteLine(result);
---
+++ Advent of Code/Resolver.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCode.Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Instantiates the correspoding AOC day class and method.
+    /// </summary>
+    internal class Resolver
+    {
+        public static (object?, MethodInfo) Resolve(int year, int day, int part, IInputProvider provider)
+        {
+            if (part != 1 && part != 2)
+                throw new Exception("Part can be 1 or 2");
+
+            var cls = Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Single(x => x.FullName == $"AdventOfCode.Y{year}.Day{day:00}");
+
+            if (cls == null)
+                throw new Exception($"AOC {year}-{day:00} is not implemented.");
+
+            var method = cls.GetMethod($"Part{part}");
+
+            if (method == null)
+                throw new Exception($"AOC {year}-{day:00} Part {part} is not implemented.");
+
+            var instance = Activator.CreateInstance(cls, new object[] { provider });
+
+            return (instance, method);
+        }
+    }
+}
