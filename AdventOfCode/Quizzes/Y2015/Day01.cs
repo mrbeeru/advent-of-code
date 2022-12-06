f91160cc89131a79c51a19523ba89b873d7f03ac
mrbeeru <andrei_bublea@yahoo.com>
2022-12-06 14:27:25 +0200

    aoc 2015 day 1

---
+++ AdventOfCode/Quizzes/Y2015/Day01.cs
@@ -0,0 +1,43 @@
+﻿using AdventOfCode.Reader;
+using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Quizzes.Y2015
+{
+    internal class Day01 : IQuizPartOne<long>, IQuizPartTwo<long>
+    {
+        private readonly IInputProvider inputProvider;
+
+        public Day01(IInputProvider inputProvider)
+        {
+            this.inputProvider = inputProvider;
+        }
+
+        public long Part1()
+        {
+            var input = inputProvider.GetInput().First();
+            return input.Count(x => x == '(') - input.Count(x => x == ')');
+        }
+
+        public long Part2()
+        {
+            var input = inputProvider.GetInput().First();
+            int sum = 0, index = 0;
+
+            foreach (var c in input)
+            {
+                sum += c == '(' ? 1 : -1;
+                index++;
+
+                if (sum < 0)
+                    break;
+            }
+
+            return index;
+        }
+    }
+}
