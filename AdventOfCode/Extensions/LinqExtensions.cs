0032d84b08812e27677302b283ff3112f510b0c0
mrbeeru <andrei_bublea@yahoo.com>
2022-12-11 12:42:18 +0200

    aoc 2022 day 11 part 2

--- AdventOfCode/Extensions/LinqExtensions.cs
+++ AdventOfCode/Extensions/LinqExtensions.cs
@@ -36,5 +36,29 @@ namespace AdventOfCode.Extensions
 
             return output;
         }
+
+        /// <summary>
+        /// Computes the product of a sequence of <see cref="long"/> elements obtained by invoking a transform function on each element of the input sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The type param.</typeparam>
+        /// <param name="source">The source sequence.</param>
+        /// <param name="selector">The selector.</param>
+        /// <returns>The product of the projected values.</returns>
+        public static long Product<TSource>(this IEnumerable<TSource> source, Func<TSource, long> selector)
+        {
+            return source.Aggregate(1L, (prod, next) => prod * selector(next));
+        }
+
+        /// <summary>
+        /// Computes the product of a sequence of <see cref="int"/> elements obtained by invoking a transform function on each element of the input sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The type param.</typeparam>
+        /// <param name="source">The source sequence.</param>
+        /// <param name="selector">The selector.</param>
+        /// <returns>The product of the projected values.</returns>
+        public static long Product<TSource>(this IEnumerable<TSource> source, Func<TSource, int> selector)
+        {
+            return source.Aggregate(1L, (prod, next) => prod * selector(next));
+        }
     }
 }
--- AdventOfCode/Quizzes/Y2022/Day11.cs
+++ AdventOfCode/Quizzes/Y2022/Day11.cs
@@ -13,7 +13,7 @@ namespace AdventOfCode.Quizzes.Y2022
     /// <summary>
     /// https://adventofcode.com/2022/day/11
     /// </summary>
-    internal class Day11 : IPartOne<long>
+    internal class Day11 : IPartOne<long>, IPartTwo<long>
     {
         private readonly IInputProvider inputProvider;
 
@@ -24,9 +24,19 @@ namespace AdventOfCode.Quizzes.Y2022
 
         public long Part1()
         {
+            return Solve(20, x => x / 3);
+        }
+
+        public long Part2()
+        {
+            return Solve(10_000, x => x % (2*3*5*7*11*13*17*19));
+        }
+
+        private long Solve(int loops, Func<long, long> stressLoweringMechanism)
+        {
             var monkeys = ParseInput(inputProvider.GetInput()).ToList();
 
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < loops; i++)
             {
                 foreach (var monkey in monkeys)
                 {
@@ -34,50 +44,21 @@ namespace AdventOfCode.Quizzes.Y2022
 
                     foreach (var item in items)
                     {
-                        var result = monkey.GetOperationResult(item);
-                        var nextMonkey = monkeys[monkey.Test(result) ? monkey.TestTrueMonkeyId : monkey.TestFalseMonkeyId];
-                        monkey.PassItemto(result, nextMonkey);
+                        var stressLevel = monkey.GetOperationResult(item);
+                        var nextMonkey = monkeys[monkey.Next(stressLevel)];
+                        monkey.PassItemto(stressLoweringMechanism(stressLevel), nextMonkey);
                         monkey.Activity++;
                     }
                     monkey.Items.Clear();
                 }
             }
 
-            return monkeys.OrderByDescending(x => x.Activity)
-                          .Take(2)
-                          .Aggregate(1L, (result, monkey) => result * monkey.Activity);
+            return monkeys.OrderByDescending(x => x.Activity).Take(2).Product(x => x.Activity);
         }
 
         private IEnumerable<Monkey> ParseInput(IEnumerable<string> input)
         {
-            var groups = input.GroupCount(7);
-            var monkeys = new List<Monkey>();
-
-            foreach (var monkeyDefinition in groups)
-            {
-                var m = monkeyDefinition.ToArray();
-                var monkeyId = int.Parse(m[0]["Monkey ".Length..^1]);
-                var items = m[1]["  Starting items: ".Length..^0].Split(", ").Select(x => long.Parse(x));
-                var operation = m[2]["  Operation: new = ".Length..^0];
-                var test = int.Parse(m[3]["  Test: divisible by ".Length..^0]);
-                var testTrueMonkeyId = int.Parse(m[4]["    If true: throw to monkey ".Length..^0]);
-                var testFalseMonkeyId = int.Parse(m[5]["    If false: throw to monkey ".Length..^0]);
-
-                var monkey = new Monkey
-                {
-                    Id = monkeyId,
-                    Items = items.ToList(),
-                    Operation = operation,
-                    TestNumber = test,
-                    TestTrueMonkeyId = testTrueMonkeyId,
-                    TestFalseMonkeyId = testFalseMonkeyId,
-
-                };
-
-                monkeys.Add(monkey);
-            }
-
-            return monkeys;
+            return input.GroupCount(7).Select(x => Monkey.Parse(x.ToArray()));
         }
 
         private class Monkey
@@ -90,30 +71,30 @@ namespace AdventOfCode.Quizzes.Y2022
             public int TestFalseMonkeyId { get; set; }
             public long Activity { get; set; }
 
-            public bool Test(long value)
+            public static Monkey Parse(string[] monkeyDefs)
             {
-                return value % TestNumber == 0;
+                return new Monkey
+                {
+                    Id = int.Parse(monkeyDefs[0]["Monkey ".Length..^1]),
+                    Items = monkeyDefs[1]["  Starting items: ".Length..^0].Split(", ").Select(x => long.Parse(x)).ToList(),
+                    Operation = monkeyDefs[2]["  Operation: new = ".Length..^0],
+                    TestNumber = int.Parse(monkeyDefs[3]["  Test: divisible by ".Length..^0]),
+                    TestTrueMonkeyId = int.Parse(monkeyDefs[4]["    If true: throw to monkey ".Length..^0]),
+                    TestFalseMonkeyId = int.Parse(monkeyDefs[5]["    If false: throw to monkey ".Length..^0]),
+                };
             }
 
             public long GetOperationResult(long item)
             {
-                var a = Operation.Split(' ');
-                var item1 = item;
-                var item2 = char.IsDigit(a[2][0]) ? long.Parse(a[2]) : item1;
-                var op = a[1][0];
-
-                return Calc(op, item1, item2) / 3;
+                var parts = Operation.Split(' ');
+                var item2 = char.IsDigit(parts[2][0]) ? long.Parse(parts[2]) : item;
+                var op = parts[1][0];
+                return Calc(op, item, item2);
             }
 
-            private long Calc(char op, long item1, long item2)
-            {
-                return op == '*' ? item1 * item2 : item1 + item2;
-            }
-
-            public void PassItemto(long item, Monkey monkey)
-            {
-                monkey.Items.Add(item);
-            }
+            public int Next(long value) => value % TestNumber == 0 ? TestTrueMonkeyId : TestFalseMonkeyId;
+            private long Calc(char op, long item1, long item2) => op == '*' ? item1 * item2 : item1 + item2;
+            public void PassItemto(long item, Monkey monkey) => monkey.Items.Add(item);
         }
     }
 }
