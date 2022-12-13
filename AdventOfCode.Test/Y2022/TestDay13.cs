cf26d41b9c7bf40ae2be080246dc5fd17372bb06
mrbeeru <andrei_bublea@yahoo.com>
2022-12-13 22:10:18 +0200

    aoc 2022 day 13 part 1

---
+++ AdventOfCode.Test/Y2022/TestDay13.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCode.Quizzes.Y2022;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Test.Y2022
+{
+    public class TestDay13 : TestBase
+    {
+        [Fact]
+        public void Part1()
+        {
+            var a = new Day13(InputProvider);
+            Assert.Equal(13, a.Part1());
+        }
+
+        protected override string[] AocInput()
+        {
+            return new[] {
+                "[1,1,3,1,1]",
+                "[1,1,5,1,1]",
+                "",
+                "[[1],[2,3,4]]",
+                "[[1],4]",
+                "",
+                "[9]",
+                "[[8,7,6]]",
+                "",
+                "[[4,4],4,4]",
+                "[[4,4],4,4,4]",
+                "",
+                "[7,7,7,7]",
+                "[7,7,7]",
+                "",
+                "[]",
+                "[3]",
+                "",
+                "[[[]]]",
+                "[[]]",
+                "",
+                "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+                "[1,[2,[3,[4,[5,6,0]]]],8,9]"
+            };
+        }
+    }
+}
---
+++ AdventOfCode/Quizzes/Y2022/Day13.cs
@@ -0,0 +1,128 @@
+﻿using AdventOfCode.Reader;
+using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Quizzes.Y2022
+{
+    public class Day13 : IPartOne<long>
+    {
+        private readonly IInputProvider inputProvider;
+
+        public Day13(IInputProvider inputProvider)
+        {
+            this.inputProvider = inputProvider;
+        }
+
+        public long Part1()
+        {
+            var input = inputProvider.GetInput();
+            var groups = input.Split(string.Empty);
+            var parsed = groups.Select(x => x.Select(y => Parse(y)).ToArray()).ToList();
+            var res = parsed.Select((x, i) => (x[0].CompareTo(x[1]), i + 1)).Where(x => x.Item1 == 1);
+            return res.Sum(x => x.Item2);
+        }
+
+        public bool Compare(string a, string b)
+        {
+            return false;
+        }
+
+        private Node Parse(string a)
+        {
+            Node root = new Node();
+            Node current = root;
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] == '[')
+                {
+                    var tmp = new Node();
+                    current.Next.Add(tmp);
+                    tmp.Parent = current;
+                    current = tmp;
+                } else if (a[i] == ']')
+                {
+                    current = current.Parent;
+                } else if (a[i] == ',')
+                {
+                    continue;
+                } else if (char.IsDigit(a[i]))
+                {
+                    var num = NextNumber(a, i);
+                    i += num/10;
+                    current.Next.Add(num);
+                } else
+                {
+                    throw new Exception("Unhandled.");
+                }
+            }
+
+            return root;
+        }
+
+        private class Node : IComparable<Node>
+        {
+            public Node Parent;
+            public List<object> Next { get; set; } = new List<object>();
+
+            public int CompareTo(Node? other)
+            {
+                for (int i = 0; i < Next.Count; i++)
+                {
+                    if (i >= other.Next.Count)
+                        return -1;
+
+                    object obj = Next[i];
+                    object obj2 = other.Next[i];
+
+                    if (obj is int numLeft && obj2 is int numRight)
+                    {
+                        if (numLeft > numRight)
+                            return -1;
+                        else if (numLeft < numRight)
+                            return 1;
+                    }
+
+                    if (obj is Node nodeLeft && obj2 is Node nodeRight)
+                    {
+                        return nodeLeft.CompareTo(nodeRight);
+                    }
+
+                    if (obj is Node)
+                    {
+                        return ((Node)obj).CompareTo(new Node { Next = new List<object> { obj2 } });
+                    }
+
+                    if (obj2 is Node)
+                    {
+                        return new Node { Next = new List<object> { obj } }.CompareTo((Node)obj2);
+                    }
+                }
+
+                return 1;
+            }
+        }
+
+        private int NextNumber(string a, int index)
+        {
+            if (!char.IsDigit(a[index]))
+                throw new Exception($"No digit at pos {index}");
+
+            int result = 0;
+
+            while (char.IsDigit(a[index]))
+            {
+                result *= 10;
+                result += a[index] - '0';
+                index++;
+            }
+
+            return result;
+        }
+
+    }
+}
