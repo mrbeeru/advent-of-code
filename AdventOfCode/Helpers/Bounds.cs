7462f379f7387a37e13a032df967e8b1958a9bc6
mrbeeru <andrei_bublea@yahoo.com>
2022-12-14 19:26:20 +0200

    added helper method for Bounds, aoc 2022 day 14 cleanup

---
+++ AdventOfCode/Helpers/Bounds.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Helpers
+{
+    internal static class Bounds
+    {
+        public static bool Contains(int width, int height, int row, int col) 
+        {
+            return col >= 0 && col < width && row >= 0 && row < height;
+        }
+
+        public static bool Contains<T>(T[][] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.Length && col >= 0 && col < matrix[^1].Length;
+        }
+    }
+}
--- AdventOfCode/Quizzes/Y2022/Day14.cs
+++ AdventOfCode/Quizzes/Y2022/Day14.cs
@@ -1,4 +1,5 @@
-﻿using AdventOfCode.Reader;
+﻿using AdventOfCode.Helpers;
+using AdventOfCode.Reader;
 using MoreLinq;
 using System.Text.RegularExpressions;
 
@@ -15,33 +16,25 @@ namespace AdventOfCode.Quizzes.Y2022
 
         public long Part1()
         {
-            var pairs = inputProvider.GetInput().Select(x => Regex.Matches(x, @"(\d+),(\d+)").Select(x => (int.Parse(x.Groups[1].Value), int.Parse(x.Groups[2].Value))));
-            (int w, int h) = FindDimensions(pairs);
-            var matrix = InitializeMatrix(pairs, w, h);
+            var matrix = InitializeMatrix();
             return Simulate(matrix, 500, 0);
         }
 
         public long Part2()
         {
-            var pairs = inputProvider.GetInput().Select(x => Regex.Matches(x, @"(\d+),(\d+)").Select(x => (int.Parse(x.Groups[1].Value), int.Parse(x.Groups[2].Value))));
-            (int w, int h) = FindDimensions(pairs);
-            var matrix = InitializeMatrix(pairs, w + h, h + 2, markFloor: true); // account for width and floor
+            var matrix = InitializeMatrix(isPart2: true);
             return Simulate(matrix, 500, 0);
         }
 
-        private (int w, int h) FindDimensions(IEnumerable<IEnumerable<(int, int)>> matches)
+        private char[][] InitializeMatrix(bool isPart2 = false)
         {
-            var maxX = matches.SelectMany(x => x.Select(y => y.Item1)).Max();
-            var maxY = matches.SelectMany(x => x.Select(y => y.Item2)).Max();
-            return (maxX + 1, maxY + 1);
-        }
+            var pairs = inputProvider.GetInput().Select(x => Regex.Matches(x, @"(\d+),(\d+)").Select(x => (int.Parse(x.Groups[1].Value), int.Parse(x.Groups[2].Value))));
+            (int w, int h) = FindDimensions(pairs, isPart2);
 
-        private char[][] InitializeMatrix(IEnumerable<IEnumerable<(int, int)>> matches, int w, int h, bool markFloor = false)
-        {
             char[][] m = new char[h][];
             Enumerable.Range(0, h).Select(x => m[x] = new char[w]).ToList();
 
-            foreach (var match in matches)
+            foreach (var match in pairs)
             {
                 (int x, int y) current = match.First();
 
@@ -57,51 +50,51 @@ namespace AdventOfCode.Quizzes.Y2022
                 }
             }
 
-            if (markFloor) // mark last row as floor with '#'
+            if (isPart2) // mark last row as floor with '#'
                 for (int i = 0; i < w; i++)
                     m[^1][i] = '#';
 
             return m;
         }
 
+        private (int w, int h) FindDimensions(IEnumerable<IEnumerable<(int, int)>> matches, bool isPart2)
+        {
+            var maxX = matches.SelectMany(x => x.Select(y => y.Item1)).Max();
+            var maxY = matches.SelectMany(x => x.Select(y => y.Item2)).Max();
+            return (maxX + 1 + (isPart2 ? maxY : 0), maxY + 1 + (isPart2 ? 2 : 0));
+        }
+
         private int Simulate(char[][] matrix, int x, int y)
         {
             (int width, int height) = (matrix[0].Length, matrix.Length);
-            (int x, int y)[] dirs = new[] { (0, 1), (-1, 1), (1, 1) };
+            (int x, int y)[] dir = new[] { (0, 1), (-1, 1), (1, 1) };
 
             for (int i = 0; ; i++)
             {
-                (int cx, int cy) = (x, y);
+                (int col, int row) = (x, y);
 
                 while (true)
                 {
-                    //condition for part 2
-                    if (matrix[cy][cx] == 'o')
+                    // either sand fell in abyss or built up to the source, we can stop here.
+                    if (row >= height - 1 || col - 1 <= 0 || col + 1 >= width - 1 || matrix[row][col] == 'o')
                         return i;
 
-                    //abyss for part 1
-                    if (cy >= height - 1 || cx - 1 <= 0 || cx + 1 >= width - 1)
-                        return i;
-
-                    var d = dirs.Select(x => (x.x + cx, x.y + cy))
-                        .Where(x => IsInBounds(x.Item1, x.Item2, width, height) && matrix[x.Item2][x.Item1] == 0)
+                    var d = dir.Select(x => (x.y + row, x.x + col))
+                        .Where(x => Bounds.Contains(matrix, x.Item1, x.Item2) && matrix[x.Item1][x.Item2] == 0)
                         .FirstOrDefault();
 
-                    if (d.Item1 != 0 && d.Item2 != 0)
+                    // this means we can't go in any direction => sand found it's resting place
+                    if (d.Item1 == 0 && d.Item2 == 0)
                     {
-                        cx = d.Item1;
-                        cy = d.Item2;
-                        continue;
+                        matrix[row][col] = 'o';
+                        break;
                     }
 
-                    matrix[cy][cx] = 'o';
-                    break;
+                    (row, col) = (d.Item1, d.Item2);
                 }
             }
 
             throw new Exception("Should return before reaching here.");
         }
-
-        private bool IsInBounds(int x, int y, int width, int height) => x >= 0 && x < width && y >= 0 && y < height;
     }
 }
