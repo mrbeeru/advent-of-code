e755bf35979fc2e3a5bcce2827129806d28d7caf
mrbeeru <andrei_bublea@yahoo.com>
2022-12-15 23:50:18 +0200

    commented extension method

--- AdventOfCode/Extensions/StringExtensions.cs
+++ AdventOfCode/Extensions/StringExtensions.cs
@@ -9,6 +9,11 @@ namespace AdventOfCode.Extensions
 {
     internal static class StringExtensions
     {
+        /// <summary>
+        /// Extracts all the <see cref="int"/> values from a string.
+        /// </summary>
+        /// <param name="str">The input.</param>
+        /// <returns>An IEnumerable containing all the <see cref="int"/> values found in input string.</returns>
         public static IEnumerable<int> Nums(this string str)
         {
             return Regex.Matches(str, @"-?\d+").Select(x => int.Parse(x.Value));
