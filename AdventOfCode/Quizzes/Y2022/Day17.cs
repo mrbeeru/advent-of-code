d869271026249dd58760027699d87424d676fe7c
mrbeeru <andrei_bublea@yahoo.com>
2022-12-18 01:55:39 +0200

    aoc 2022 day 17 improved code

--- AdventOfCode/Quizzes/Y2022/Day17.cs
+++ AdventOfCode/Quizzes/Y2022/Day17.cs
@@ -1,9 +1,4 @@
 ﻿using AdventOfCode.Reader;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode.Quizzes.Y2022
 {
@@ -18,28 +13,23 @@ namespace AdventOfCode.Quizzes.Y2022
 
         public long Part1()
         {
-            var matrix = new int[10_000, 7];
             var sequence = inputProvider.GetInput().First();
-            int jetCounter = 0;
-            int maxRow = 0;
+            var matrix = new int[10_000, 7];
+            int jetCounter = 0, maxRow = 0;
 
             for (int i = 0; i < 2022; i++)
             {
-                var nextShape = Shape.Create(i % 5);
-                nextShape.Col = 2;
-                nextShape.Row = maxRow + 3;
+                var nextShape = Shape.Create(i % 5, maxRow + 3, 2);
 
                 while (true)
                 {
                     var nextJet = sequence[(jetCounter++) % sequence.Length];
                     JetMovesRock(nextShape, nextJet, matrix);
-                    var test = GravityMovesRock(nextShape, matrix);
-
-                    if (test)
-                        continue;
-                    
-                    maxRow = Math.Max(nextShape.Cement(matrix) + 1, maxRow);
-                    break;
+                    if (!GravityMovesRock(nextShape, matrix))
+                    {
+                        maxRow = Math.Max(Cement(matrix, nextShape) + 1, maxRow);
+                        break;
+                    }
                 }
             }
 
@@ -52,56 +42,17 @@ namespace AdventOfCode.Quizzes.Y2022
             return 1504093567249;
         }
 
-        private void PrintMatrix(int[,] matrix, int from, int to)
-        {
-            Console.Clear();
-            for (int i = to; i >= from; i--)
-            {
-                for (int j = 0; j < 7; j++)
-                {
-                    Console.Write(matrix[i, j] == 0 ? '.' : "#");
-                }
-
-                Console.WriteLine();
-            }
-        }
-
         private void JetMovesRock(Shape shape, char jet, int[,] matrix)
         {
-            bool ok = true;
+            var colDir = jet == '<' ? -1 : 1;
 
+            //check bounds
+            if (shape.Col + colDir < 0 || shape.Col + shape.Pos.Max(x => x.Item2) + colDir > 6)
+                return;
 
-            if (jet == '<')
-            {
-                // check left wall
-                if (shape.Col <= 0)
-                    return;
-
-                foreach (var coords in shape.Pos)
-                {
-                    if (matrix[shape.Row + coords.Item1, shape.Col + coords.Item2 - 1] != 0)
-                        ok = false;
-                }
-
-                if (ok)
-                    shape.Col--;
-            }
-            else if (jet == '>')
-            {
-                //check right wall
-                if (shape.Col + shape.Pos.Max(x => x.Item2) >= 6)
-                    return;
-
-                foreach (var coords in shape.Pos)
-                {
-                    if (matrix[shape.Row + coords.Item1, shape.Col + coords.Item2 + 1] != 0)
-                        ok = false;
-                }
-
-                if (ok)
-                    shape.Col++;
-            }
-            else throw new Exception("Invalid jet type.");
+            //check collisions between shapes horizontally
+            if (shape.Pos.All(coord => matrix[shape.Row + coord.Item1, shape.Col + coord.Item2 + colDir] == 0))
+                shape.Col += colDir;
         }
 
         private bool GravityMovesRock(Shape shape, int[,] matrix)
@@ -109,53 +60,41 @@ namespace AdventOfCode.Quizzes.Y2022
             if (shape.Row == 0)
                 return false;
 
-            bool ok = true;
-            foreach (var coords in shape.Pos)
-            {
-                if (matrix[shape.Row + coords.Item1 - 1, shape.Col + coords.Item2] != 0)
-                {
-                    ok = false;
-                }
-            }
-
-            if (ok)
+            // check collisions between shapes with below row
+            if (shape.Pos.All(coords => matrix[shape.Row + coords.row - 1, shape.Col + coords.col] == 0))
             {
                 shape.Row--;
+                return true;
             }
 
-            return ok;
+            return false;
         }
 
+        private int Cement(int[,] matrix, Shape shape)
+        {
+            foreach (var pos in shape.Pos)
+                matrix[shape.Row + pos.row, shape.Col + pos.col] = 1; // 1 marks as cemented
 
+            return shape.Row + shape.Pos.Select(x => x.row).Max(); // returns top row where this shape is cemented
+        }
 
         private class Shape
         {
             public int Row { get; set; }
             public int Col { get; set; }
+            public List<(int row, int col)> Pos { get; set; }
 
-            public List<(int, int)> Pos { get; set; }
-
-            public static Shape Create(int kind, params (int, int)[] pos)
+            public static Shape Create(int kind, int row, int col)
             {
                 return kind switch
                 {
-                    0 => new Shape() { Pos = new[] { (0, 0), (0, 1), (0, 2), (0, 3) }.ToList() },
-                    1 => new Shape() { Pos = new[] { (0, 1), (1, 0), (1, 1), (1, 2), (2, 1) }.ToList() },
-                    2 => new Shape() { Pos = new[] { (0, 0), (0, 1), (0, 2), (1, 2), (2, 2) }.ToList() },
-                    3 => new Shape() { Pos = new[] { (0, 0), (1, 0), (2, 0), (3, 0) }.ToList() },
-                    4 => new Shape() { Pos = new[] { (0, 0), (0, 1), (1, 0), (1, 1) }.ToList() },
+                    0 => new Shape() { Row = row, Col = col, Pos = new[] { (0, 0), (0, 1), (0, 2), (0, 3) }.ToList() },
+                    1 => new Shape() { Row = row, Col = col, Pos = new[] { (0, 1), (1, 0), (1, 1), (1, 2), (2, 1) }.ToList() },
+                    2 => new Shape() { Row = row, Col = col, Pos = new[] { (0, 0), (0, 1), (0, 2), (1, 2), (2, 2) }.ToList() },
+                    3 => new Shape() { Row = row, Col = col, Pos = new[] { (0, 0), (1, 0), (2, 0), (3, 0) }.ToList() },
+                    4 => new Shape() { Row = row, Col = col, Pos = new[] { (0, 0), (0, 1), (1, 0), (1, 1) }.ToList() },
                     _ => throw new Exception("Invalid shape kind.")
                 };
-            }
-
-            public int Cement(int[,] matrix)
-            {
-                foreach (var pos in Pos)
-                {
-                    matrix[Row + pos.Item1, Col + pos.Item2] = 1;
-                }
-
-                return Row + Pos.Select(x => x.Item1).Max();
             }
         }
     }
