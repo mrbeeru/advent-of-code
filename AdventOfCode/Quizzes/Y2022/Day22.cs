20d241448870c04ec2a48c95ed540005b4bdc712
mrbeeru <andrei_bublea@yahoo.com>
2022-12-23 01:05:18 +0200

    refactored aoc 2022 day 22 part 1

--- AdventOfCode/Quizzes/Y2022/Day22.cs
+++ AdventOfCode/Quizzes/Y2022/Day22.cs
@@ -2,15 +2,18 @@
 using AdventOfCode.Reader;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static QuikGraph.Algorithms.Assignment.HungarianAlgorithm;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace AdventOfCode.Quizzes.Y2022
 {
-    public class Day22 : IPartOne<long>
+    public class Day22 : IPartOne<long>, IPartTwo<long>
     {
         private readonly IInputProvider inputProvider;
 
@@ -19,21 +22,68 @@ namespace AdventOfCode.Quizzes.Y2022
         public long Part1()
         {
             var lines = inputProvider.GetInput();
-            var map = BuildMap(lines);
+            var map = lines.Select(x => x.ToArray()).ToArray();
             var path = Regex.Split(lines.Last(), "(?=[RL])");
-            (int row, int col) start = FindStartPosition(map);
+            var (pos, dir) = Simulate(map, path);
 
-            var (pos, dir) = Simulate(map, path, start);
-
-            return 1000 * (pos.row + 1) + 4 * (pos.col + 1) + DirScore(dir);
+            return 1000 * pos.row + 4 * pos.col + DirScore(dir);
         }
 
-        private ((int row, int col), (int row, int col)) Simulate(char[,] map, string[] paths, (int row, int col) start)
+        public long Part2()
+        {
+            return 0;
+        }
+
+        private Face BuildFaces(char[][] arr)
+        {
+            var f1 = new Face()
+            {
+                Data = arr[0..50].Select(x => x[50..100]).ToArray(),
+                Offset = (0, 50)
+            };
+
+            var f2 = new Face() { 
+                Data = arr[0..50].Select(x => x[100..150]).ToArray(),
+                Offset = (0, 100)
+            };
+
+            var f3 = new Face() { 
+                Data = arr[50..100].Select(x => x[50..100]).ToArray(),
+                Offset = (50, 50)
+            };
+
+            var f4 = new Face() {
+                Data = arr[100..150].Select(x => x[0..50]).ToArray(),
+                Offset = (100, 0)
+            };
+
+            var f5 = new Face() {
+                Data = arr[100..150].Select(x => x[50..100]).ToArray(),
+                Offset = (100, 50)
+            };
+
+            var f6 = new Face() {
+                Data = arr[150..200].Select(x => x[0..50]).ToArray(),
+                Offset = (150, 0)
+            };
+
+            f1.Adjacent(f5, f3, f2, f2);
+            f2.Adjacent(f2, f2, f1, f1);
+            f3.Adjacent(f1, f5, f3, f3);
+            f4.Adjacent(f6, f6, f5, f5);
+            f5.Adjacent(f3, f1, f4, f4);
+            f6.Adjacent(f4, f4, f6, f6);
+
+            return f1;
+        }
+
+        private ((int row, int col), (int row, int col)) Simulate(char[][] map, string[] paths)
         {
             (int row, int col) dir = (0, 1);
-            //var ndir = dir;
+            (int row, int col) pos = (0, 0);
             var steps = int.Parse(paths[0]);
-            var pos = start;
+            var face = BuildFaces(map);
+            var nface = face;
 
             foreach (var path in paths)
             {
@@ -45,62 +95,48 @@ namespace AdventOfCode.Quizzes.Y2022
 
                 for (int i = 0; i < steps; i++)
                 {
+                    nface = face;
                     (int row, int col) npos = (pos.row + dir.row, pos.col + dir.col);
-                    npos = (Mod(npos.row, map.GetLength(0)), Mod(npos.col, map.GetLength(1)));
 
-                    if (map[npos.row, npos.col] == '#')
+                    if (npos.row < 0)
+                        nface = nface.Top;
+
+                    if (npos.row >= face.Data.Length)
+                        nface = nface.Bot;
+
+                    if (npos.col < 0)
+                        nface = nface.Left;
+
+                    if (npos.col >= 50)
+                        nface = nface.Right;
+
+                    npos = (Mod(npos.row, nface.Data.Length), Mod(npos.col, nface.Data.Length));
+
+                    if (nface[npos.row, npos.col] == '#')
                         break;
 
-                    if (map[npos.row, npos.col] == ' ')
-                    {
-                        var t = WrapAround(map, npos, dir);
-                        if (t != npos)
-                        {
-                            pos = t;
-                        }
-
-                        continue;
-                    }
-
+                    face = nface;
                     pos = npos;
                 }
 
-                Console.WriteLine((pos.col + 1, pos.row + 1));
+                //Console.WriteLine((pos.col + 1 + face.Offset.col, pos.row + 1 + face.Offset.row));
             }
 
-            return (pos, dir);
-        }
-
-        private (int row, int col) WrapAround(char[,] map, (int row, int col) pos, (int row, int col) dir)
-        {
-            var next = pos;
-            
-            while (map[next.row, next.col] == ' ')
-            {
-                next = (next.row + dir.row, next.col + dir.col);
-                next = (Mod(next.row, map.GetLength(0)), Mod(next.col, map.GetLength(1)));
-            }
-
-            if (map[next.row, next.col] == '.')
-                pos = next;
-
-            return pos;
+            return ((pos.row + face.Offset.row + 1, pos.col + face.Offset.col + 1), dir);
         }
 
         private (int row, int col) ChangeDirection(char dir, (int row, int col) currentDir)
         {
             return (dir, currentDir) switch
             {
-                ('R', ( 0,  1)) => ( 1,  0),
-                ('R', ( 1,  0)) => ( 0, -1),
-                ('R', ( 0, -1)) => (-1,  0),
-                ('R', (-1,  0)) => ( 0,  1),
-                
-                ('L', ( 0,  1)) => (-1,  0),
-                ('L', (-1,  0)) => ( 0, -1),
-                ('L', ( 0, -1)) => ( 1,  0),
-                ('L', ( 1,  0)) => ( 0,  1),
-
+                ('R', (0, 1)) => (1, 0),
+                ('R', (1, 0)) => (0, -1),
+                ('R', (0, -1)) => (-1, 0),
+                ('R', (-1, 0)) => (0, 1),
+                ('L', (0, 1)) => (-1, 0),
+                ('L', (-1, 0)) => (0, -1),
+                ('L', (0, -1)) => (1, 0),
+                ('L', (1, 0)) => (0, 1),
                 _ => throw new Exception("Invalid direction.")
             };
         }
@@ -119,50 +155,20 @@ namespace AdventOfCode.Quizzes.Y2022
 
         private int Mod(int x, int m) => (x%m + m)%m;
 
-        private char[,] BuildMap(IEnumerable<string> input)
+        private class Face
         {
-            var lines = input.ToArray()[0..^2];
-            var rows = lines.Count();
-            var cols = lines.Select(x => x.Length).Max();
+            public char this[int row, int col] => Data[row][col];
+            public char[][] Data { get; set; }
+            public (int row, int col) Offset { get; set; }
+            public Face Top { get; set; }
+            public Face Bot { get; set; }
+            public Face Left { get; set; }
+            public Face Right { get; set; }
 
-            var map = new char[rows, cols];
-
-            for (int i = 0; i < rows; i++)
+            public void Adjacent(Face top, Face bot, Face left, Face right)
             {
-                for (int j = 0; j < cols; j++)
-                {
-                    if (j < lines.ElementAt(i).Count())
-                        map[i, j] = lines[i][j];
-                    else
-                        map[i, j] = ' ';
-                    
-                }
+                Top = top; Bot = bot; Left = left; Right = right;
             }
-
-            return map;
-        }
-
-        private void PrintMap(char[,] map)
-        {
-            for (int i = 0; i < map.GetLength(0); i++)
-            {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    Console.Write(map[i,j]);
-                };
-                Console.WriteLine();
-            }
-        }
-
-        private (int row, int col) FindStartPosition(char[,] map)
-        {
-            for (int j = 0; j < map.GetLength(1); j++)
-            {
-                if (map[0, j] == '.')
-                    return (0, j);
-            }
-
-            throw new Exception("Start positin not found.");
         }
     }
 }
