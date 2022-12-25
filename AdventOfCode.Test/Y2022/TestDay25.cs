28be342ec6d21c6e2a569ac7845d2ff4ca80983d
mrbeeru <andrei_bublea@yahoo.com>
2022-12-25 12:41:48 +0200

    added aoc 2022 day 25

---
+++ AdventOfCode.Test/Y2022/TestDay25.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode.Quizzes.Y2022;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Test.Y2022
+{
+    public class TestDay25 : TestBase
+    {
+        [Fact]
+        public void Part1() => Assert.Equal("2=-1=0", new Day25(InputProvider).Part1());
+
+        protected override string[] AocInput()
+        {
+            return new[]
+            {
+                "1=-0-2",
+                "12111",
+                "2=0=",
+                "21",
+                "2=01",
+                "111",
+                "20012",
+                "112",
+                "1=-1=",
+                "1-12",
+                "12",
+                "1=",
+                "122",
+            };
+        }
+    }
+}
---
+++ AdventOfCode/Quizzes/Y2022/Day25.cs
@@ -0,0 +1,73 @@
+﻿using AdventOfCode.Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Quizzes.Y2022
+{
+    public class Day25 : IPartOne<string>
+    {
+        IInputProvider inputProvider;
+
+        public Day25(IInputProvider inputProvider) => this.inputProvider = inputProvider;
+
+        public string Part1()
+        {
+            var input = inputProvider.GetInput();
+            var sum = input.Aggregate(0L, (x, next) => x + SnafuToDecimal(next));
+            return DecimalToSnafu(sum);
+        }
+
+        public long SnafuToDecimal(string snafu)
+        {
+            var dec = 0L;
+
+            for (int i = 0; i < snafu.Length; i++)
+            {
+                var digit = snafu[snafu.Length - 1 - i] switch
+                {
+                    '=' => -2,
+                    '-' => -1,
+                    '0' => 0,
+                    '1' => 1,
+                    '2' => 2,
+                    _ => throw new Exception("Illegal snafu digit.")
+                };
+
+                dec += digit * (long)Math.Pow(5, i);
+            }
+
+            return dec;
+        }
+
+        public string DecimalToSnafu(long dec)
+        {
+            var snafu = "";
+            int carry = 0;
+
+            while (dec > 0)
+            {
+                var mod = (dec % 5);
+                mod += carry;
+                dec /= 5;
+
+                var snafuDigit = mod switch
+                {
+                    0 => '0',
+                    1 => '1',
+                    2 => '2',
+                    3 => '=',
+                    4 => '-',
+                    5 => '0'
+                };
+
+                snafu += snafuDigit;
+                carry = mod >= 3 ? 1 : 0;
+            }
+
+            return string.Join("", snafu.Reverse());
+        }
+    }
+}
