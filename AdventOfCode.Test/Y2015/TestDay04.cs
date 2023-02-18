93ce61e52a219ac052433154b9e9bf0354cc109e
mrbeeru <andrei_bublea@yahoo.com>
2023-02-18 12:20:06 +0200

    aoc 2015 day 4

---
+++ AdventOfCode.Test/Y2015/TestDay04.cs
@@ -0,0 +1,31 @@
+﻿using AdventOfCode.Quizzes.Y2015;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Test.Y2015
+{
+    public class TestDay04 : TestBase
+    {
+        [Fact]
+        public void Part1()
+        {
+            var day = new Day04(InputProvider);
+            Assert.Equal(1048970, day.Part1());
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var day = new Day04(InputProvider);
+            Assert.Equal(5714438, day.Part2());
+        }
+
+        protected override string[] AocInput()
+        {
+            return new[] { "pqrstuv" };
+        }
+    }
+}
---
+++ AdventOfCode/Quizzes/Y2015/Day04.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCode.Reader;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Quizzes.Y2015
+{
+    public class Day04 : IPartOne<long>, IPartTwo<long>
+    {
+        private readonly IInputProvider inputProvider;
+
+        public Day04(IInputProvider inputProvider)
+        {
+            this.inputProvider = inputProvider;
+        }
+
+        public long Part1()
+        {
+            var key = inputProvider.GetInput().Single();
+            return FindAnswer(key, "00000");
+        }
+
+        public long Part2()
+        {
+            var key = inputProvider.GetInput().Single();
+            return FindAnswer(key, "000000");
+        }
+
+        private long FindAnswer(string key, string condition)
+        {
+            int iteration = 0;
+            using MD5 md5 = MD5.Create();
+
+            while (true)
+            {
+                var answer = $"{key}{iteration}";
+                byte[] inputBytes = Encoding.ASCII.GetBytes(answer);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                var result = Convert.ToHexString(hashBytes);
+                if (result.StartsWith(condition))
+                    return iteration;
+
+                iteration++;
+            }
+        }
+    }
+}
