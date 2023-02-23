f875487a39f9af2f9a08e235d638ffd68ffe638d
mrbeeru <andrei_bublea@yahoo.com>
2023-02-23 23:59:32 +0200

    aoc 2015 day 5

---
+++ AdventOfCode.Test/Y2015/TestDay05.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Quizzes.Y2015;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Test.Y2015
+{
+    public class TestDay05 : TestBase
+    {
+        [Fact]
+        public void Part1()
+        {
+            Assert.Equal(2, new Day05(InputProvider).Part1());
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            Assert.Equal(2, new Day05(InputProvider).Part2());
+        }
+
+        protected override string[] AocInput()
+        {
+            return new[] {
+                "ugknbfddgicrmopn",
+                "aaa",
+                "jchzalrnumimnmhp",
+                "haegwjzuvuyypxyu",
+                "dvszwmarrgswjxmb",
+                "qjhvhtzxzqqjkmpb",
+                "xxyxx",
+                "uurcxstgmygtbstg",
+                "ieodomkazucvgmuy",
+            };
+        }
+    }
+}
---
+++ AdventOfCode/Quizzes/Y2015/Day05.cs
@@ -0,0 +1,75 @@
+﻿using AdventOfCode.Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MoreLinq.Extensions.PairwiseExtension;
+using static MoreLinq.Extensions.WindowExtension;
+
+namespace AdventOfCode.Quizzes.Y2015
+{
+    public class Day05 : IPartOne<long>, IPartTwo<long>
+    {
+        private readonly IInputProvider inputProvider;
+        private delegate bool Rule(string input);
+
+        public Day05(IInputProvider inputProvider)
+        {
+            this.inputProvider = inputProvider;
+        }
+
+        public long Part1()
+        {
+            var input = inputProvider.GetInput();
+            var rules = new[] { VowelsRule, TwiceRule, ForbiddenRule };
+
+            return input.Where(str => rules.All(rule => rule.Invoke(str))).Count();
+        }
+
+        public long Part2()
+        {
+            var input = inputProvider.GetInput();
+            var rules = new[] { RepeatsRule, PairNoOverlapRule };
+
+            return input.Where(str => rules.All(rule => rule.Invoke(str))).Count();
+        }
+
+        private bool VowelsRule(string input)
+        {
+            List<char> vowles = new() { 'a', 'e', 'i', 'o', 'u' };
+            return input.Count(vowles.Contains) >= 3;
+        }
+
+        private bool TwiceRule(string input)
+        {
+            return input.Window(2).Any(x => x[0] == x[1]);
+        }
+
+        private bool ForbiddenRule(string input)
+        {
+            List<string> forbidden = new() { "ab", "cd", "pq", "xy" };
+            return forbidden.All(x => input.Contains(x) == false);
+        }
+
+        private bool RepeatsRule(string input)
+        {
+            return input.Window(3).Any(x => x[0] == x[2]);
+        }
+
+        private bool PairNoOverlapRule(string input)
+        {
+            var pairs = input.Window(2).Select(x => $"{x[0]}{x[1]}").ToList();
+            var found = false;
+
+            for (int i = 0; i < pairs.Count; i++)
+            {
+                var pair = pairs[i];
+                found |= new string(input.Skip(i + 2).ToArray()).Contains(pair);
+            }
+
+
+            return found;
+        }
+    }
+}
