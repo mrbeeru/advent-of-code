0dfa5531e94074f9560ba9003703d74f7c56a50b
mrbeeru <andrei_bublea@yahoo.com>
2023-12-01 10:38:46 +0200

    aoc 2023 day 1

---
+++ AdventOfCode.Test/Y2023/TestDay01.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCode.Quizzes.Y2023;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Test.Y2023
+{
+    public class TestDay01 : TestBase
+    {
+        [Fact]
+        public void Part1()
+        {
+            var a = new Day01(InputProvider);
+            Assert.Equal(209, a.Part1());
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var a = new Day01(InputProvider);
+            Assert.Equal(278, a.Part2());
+        }
+
+        protected override string[] AocInput()
+        {
+            return new string[]
+            {
+                "two1nine",
+                "eightwothree0",
+                "abcone2threexyz",
+                "xtwone3four",
+                "4nineeightseven2",
+                "zoneight234",
+                "7pqrstsixteen",
+            };
+        }
+    }
+}
