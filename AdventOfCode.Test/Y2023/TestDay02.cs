ccb5acd57f98100c79edc4441f645f522f6de707
mrbeeru <andrei_bublea@yahoo.com>
2023-12-02 10:36:42 +0200

    aoc 2023 day 2

---
+++ AdventOfCode.Test/Y2023/TestDay02.cs
@@ -0,0 +1,37 @@
+﻿using AdventOfCode.Quizzes.Y2023;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Test.Y2023
+{
+    public class TestDay02 : TestBase
+    {
+        [Fact]
+        public void Part1()
+        {
+            var a = new Day02(InputProvider);
+            Assert.Equal(8, a.Part1());
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var a = new Day02(InputProvider);
+            Assert.Equal(2286, a.Part2());
+        }
+
+        protected override string[] AocInput()
+        {
+            return new string[] {
+                "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+                "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+                "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+                "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+                "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+            };
+        }
+    }
+}
---
+++ AdventOfCode/Quizzes/Y2023/Day02.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCode.Reader;
+using MoreLinq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Quizzes.Y2023
+{
+    public class Day02 : IPartOne<long>, IPartTwo<long>
+    {
+        private readonly IInputProvider inputProvider;
+
+        public Day02(IInputProvider inputProvider)
+        {
+            this.inputProvider = inputProvider;
+        }
+
+        public long Part1()
+        {
+            var input = inputProvider.GetInput();
+           
+            var result = input.Select(MaxGameBallsByColor)
+                .Select((map, i) => (map, i))
+                .Where(x => x.map["red"] <= 12 && x.map["green"] <= 13 && x.map["blue"] <= 14)
+                .Sum(x => x.i + 1);
+
+            return result;
+        }
+
+        public long Part2()
+        {
+            var input = inputProvider.GetInput();
+
+            var result = input.Select(MaxGameBallsByColor)
+                .Select(map => map["red"] * map["green"] * map["blue"])
+                .Sum();
+
+            return result;
+        }
+
+        static Dictionary<string, int> MaxGameBallsByColor(string game)
+        {
+            var matches = Regex.Matches(game, @"(\d+) (red|green|blue)");
+
+            //an array like: [("red", 3), ("blue", 1)]
+            var balls = matches.Select(x => new { Color = x.Groups[2].Value, Count = int.Parse(x.Groups[1].Value) });
+
+            var map = new Dictionary<string, int> { { "red", 1 }, { "green", 1 }, { "blue", 1 } };
+            balls.ForEach((b) => map[b.Color] = Math.Max(map[b.Color], b.Count));
+
+            return map;
+        }
+    }
+}
