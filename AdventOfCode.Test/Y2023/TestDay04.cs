9a679a1d5f085702642eacfe647b98daeeed8a3e
mrbeeru <andrei_bublea@yahoo.com>
2023-12-04 08:58:21 +0200

    aoc 2023 day 4

---
+++ AdventOfCode.Test/Y2023/TestDay04.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Quizzes.Y2023;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Test.Y2023
+{
+    public class TestDay04 : TestBase
+    {
+        [Fact]
+        public void Part1()
+        {
+            var day = new Day04(InputProvider);
+            Assert.Equal(13, day.Part1());
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var day = new Day04(InputProvider);
+            Assert.Equal(30, day.Part2());
+        }
+
+
+        protected override string[] AocInput()
+        {
+            return new string[] {
+                "Card 1: 41 48 83 86 17 0 0 0 0 0 | 83 86  6 31 17  9 48 53",
+                "Card 2: 13 32 20 16 61 0 0 0 0 0 | 61 30 68 82 17 32 24 19",
+                "Card 3:  1 21 53 59 44 0 0 0 0 0 | 69 82 63 72 16 21 14  1",
+                "Card 4: 41 92 73 84 69 0 0 0 0 0 | 59 84 76 51 58  5 54 83",
+                "Card 5: 87 83 26 28 32 0 0 0 0 0 | 88 30 70 12 93 22 82 36",
+                "Card 6: 31 18 13 56 72 0 0 0 0 0 | 74 77 10 23 35 67 36 11",
+            };
+        }
+    }
+}
---
+++ AdventOfCode/Quizzes/Y2023/Day04.cs
@@ -0,0 +1,51 @@
+﻿using AdventOfCode.Extensions;
+using AdventOfCode.Helpers;
+using AdventOfCode.Reader;
+using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Quizzes.Y2023
+{
+    public class Day04 : IPartOne<long>, IPartTwo<long>
+    {
+        private readonly IInputProvider inputProvider;
+
+        public Day04(IInputProvider inputProvider)
+        {
+            this.inputProvider = inputProvider;
+        }
+
+        public long Part1()
+        {
+            return inputProvider.GetInput()
+                .Select(line => line.Nums())
+                .Select(nums => (nums.Skip(1).Take(10), nums.Skip(11)))
+                .Select(pair => pair.Item1.Intersect(pair.Item2).Count())
+                .Select(val => val == 0 ? 0 : 1 << (val - 1))
+                .Sum();
+        }
+
+        public long Part2()
+        {
+            var lines = inputProvider.GetInput().Reverse();
+            var dict = new Dictionary<int, int>();
+
+            foreach (var (line, i) in lines.Select((line, i) => (line, i)))
+            {
+                var nums = line.Nums();
+                var winning = nums.Skip(1).Take(10);
+                var cardNums = nums.Skip(11);
+                var intersectionCount = winning.Intersect(cardNums).Count();
+                var sum = dict.Where(x => x.Key <= i - 1 && x.Key > i - 1 - intersectionCount).Sum(x => x.Value);
+                dict.Add(i, sum + 1);
+            }
+
+            return dict.Values.Sum();
+        }
+    }
+}
