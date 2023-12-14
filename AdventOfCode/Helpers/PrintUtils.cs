21b0b72752c352a3455f18f119db8a81891f978c
mrbeeru <andrei_bublea@yahoo.com>
2023-12-14 18:55:24 +0200

    aoc 2023 day 14 part 1

--- AdventOfCode/Helpers/PrintUtils.cs
+++ AdventOfCode/Helpers/PrintUtils.cs
@@ -16,6 +16,26 @@ namespace AdventOfCode.Helpers
                 Console.WriteLine();
                 Debug.WriteLine("");
             }
+
+            Console.WriteLine();
+            Debug.WriteLine("");
+        }
+
+        public static void Print<T>(this T[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write(matrix[i][j]);
+                    Debug.Write(matrix[i][j]);
+                }
+                Console.WriteLine();
+                Debug.WriteLine("");
+            }
+
+            Console.WriteLine();
+            Debug.WriteLine("");
         }
     }
 }
--- AdventOfCode/Quizzes/Y2023/Day13.cs
+++ AdventOfCode/Quizzes/Y2023/Day13.cs
@@ -1,137 +1,190 @@
-﻿using AdventOfCode.Helpers;
-using AdventOfCode.Reader;
+﻿using AdventOfCode.Reader;
+using MoreLinq;
 
 namespace AdventOfCode.Quizzes.Y2023
 {
-    [Aoc(year: 2023, day: 14)]
-    public class Day14(IInputProvider inputProvider) : IPartOne<long>
+    [Aoc(year: 2023, day: 13)]
+    public class Day13(IInputProvider inputProvider) : IPartOne<long>, IPartTwo<long>
     {
         public long Part1()
         {
-            var matrix = inputProvider.GetInput().Select(x => x.ToArray()).ToArray();
-            Tilt(matrix, Coords2D.Up);
-            return Sum(matrix);
-        }
-
-        static void Tilt(char[][] matrix, Coords2D dir)
-        {
-
-            if (dir == Coords2D.Up)
+            var input = inputProvider.GetInput().Split("").Select(x => x.Select(y => y.ToArray()).ToArray()).ToArray();
+            var sum = 0;
+            foreach (var mirror in input)
             {
-                for (var i = 0; i < matrix.Length; i++)
+                var (hlines, vlines) = GetPotentialLines(mirror);
+                var hline = hlines.Where(x => VerifyHLine(x, mirror));
+                var vline = vlines.Where(x => VerifyVLine(x, mirror));
+
+                if (hline.Any())
                 {
-                    for (var j = 0; j < matrix[i].Length; j++)
-                    {
-                        if (matrix[i][j] == 'O')
-                        {
-                            for (var k = i - 1; k >= 0; k--)
-                            {
-                                if (matrix[k][j] == '.')
-                                {
-                                    matrix[k][j] = 'O';
-                                    matrix[k + 1][j] = '.';
-                                }
-                                else
-                                {
-                                    break;
-                                }
-                            }
-                        }
-                    }
+                    sum += 100 * (hline.First() + 1);
                 }
-            }
 
-            if (dir == Coords2D.Left)
-            {
-                for (var i = 0; i < matrix.Length; i++)
-                {
-                    for (var j = 0; j < matrix[i].Length; j++)
-                    {
-                        if (matrix[i][j] == 'O')
-                        {
-                            for (var k = j - 1; j >= 0; k--)
-                            {
-                                if (matrix[i][k] == '.')
-                                {
-                                    matrix[i][k] = 'O';
-                                    matrix[i][j + 1] = '.';
-                                }
-                                else
-                                {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
 
-            if (dir == Coords2D.Down)
-            {
-                for (var i = matrix.Length - 1; i >= 0; i--)
+                if (vline.Any())
                 {
-                    for (var j = matrix[i].Length - 1; j >= 0; j--)
-                    {
-                        if (matrix[i][j] == 'O')
-                        {
-                            for (var k = i + 1; k < matrix.Length; k++)
-                            {
-                                if (matrix[k][j] == '.')
-                                {
-                                    matrix[k][j] = 'O';
-                                    matrix[k - 1][j] = '.';
-                                }
-                                else
-                                {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (dir == Coords2D.Right)
-            {
-                for (var i = matrix.Length - 1; i >= 0; i--)
-                {
-                    for (var j = matrix[i].Length - 1; j >= 0; j--)
-                    {
-                        if (matrix[i][j] == 'O')
-                        {
-                            for (var k = j + 1; k < matrix[i].Length; k++)
-                            {
-                                if (matrix[i][k] == '.')
-                                {
-                                    matrix[i][k] = 'O';
-                                    matrix[i][j - 1] = '.';
-                                }
-                                else
-                                {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        static long Sum(char[][] matrix)
-        {
-            long sum = 0;
-            for (var i = 0; i < matrix.Length; i++)
-            {
-                for (var j = 0; j < matrix[i].Length; j++)
-                {
-                    if (matrix[i][j] == 'O')
-                    {
-                        sum += (matrix.Length - i);
-                    }
+                    sum += (vline.First() + 1);
                 }
             }
 
             return sum;
         }
+
+        public long Part2()
+        {
+            var input = inputProvider.GetInput().Split("").Select(x => x.Select(y => y.ToArray()).ToArray()).ToArray();
+            var sum = 0;
+            foreach (var mirror in input)
+            {
+                var (hlines1, vlines1) = GetPotentialLines(mirror);
+                var (hlines2, vlines2) = GetPotentialLines(mirror, 1);
+
+                if (hlines2.Count > hlines1.Count)
+                {
+                    hlines2 = hlines2.Except(hlines1).ToList();
+                }
+
+                if (vlines2.Count > vlines1.Count)
+                {
+                    vlines2 = vlines2.Except(vlines1).ToList();
+                }
+
+                //var hlines = hlines2.Except(hlines1);
+                //var vlines = vlines2.Except(vlines1);
+
+                var a = hlines2.Where(x => VerifyHLine(x, mirror, 1)).ToList();
+                var b = vlines2.Where(x => VerifyVLine(x, mirror, 1)).ToList();
+
+
+                if (a.Any())
+                {
+                    sum += 100 * (a.First() + 1);
+                }
+
+                if (b.Any())
+                {
+                    sum += (b.First() + 1);
+                }
+            }
+
+            return sum;
+        }
+
+        static bool VerifyHLine(int line, char[][] matrix, int flips = 0)
+        {
+            int offset = 0;
+            int numFlips = 0;
+
+            while (true)
+            {
+                for (int j = 0; j < matrix[line].Length; j++)
+                {
+                    if (matrix[line + offset + 1][j] != matrix[line - offset][j])
+                    {
+                        numFlips++;
+                    }
+
+                    if (numFlips > flips)
+                    {
+                        return false;
+                    }
+                }
+
+                offset++;
+
+                if (line + offset + 1 == matrix.Length)
+                {
+                    return flips == numFlips;
+                }
+
+                if (line - offset == -1)
+                {
+                    return flips == numFlips;
+                }
+
+            }
+        }
+
+        static bool VerifyVLine(int line, char[][] matrix, int flips = 0)
+        {
+            int offset = 0;
+            int numFlips = 0;
+
+            while (true)
+            {
+
+
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    if (matrix[j][line + offset + 1] != matrix[j][line - offset])
+                    {
+                        numFlips++;
+                    }
+
+                    if (numFlips > flips)
+                    {
+                        return false;
+                    }
+                }
+
+                offset++;
+
+
+                if (line + offset + 1 == matrix[0].Length)
+                {
+                    return flips == numFlips;
+                }
+
+                if (line - offset == -1)
+                {
+                    return flips == numFlips;
+                }
+            }
+        }
+
+        static (List<int> hLines, List<int> vLines) GetPotentialLines(char[][] mirror, int allowedDiffs = 0)
+        {
+            var hLines = new List<int>();
+            var vLines = new List<int>();
+
+            for (var i = 0; i < mirror.Length - 1; i++)
+            {
+                var diffs = 0;
+
+                // check horizontal
+                for (var j = 0; j < mirror[i].Length; j++)
+                {
+
+                    if (mirror[i][j] != mirror[i+1][j])
+                    {
+                        diffs++;
+                    }
+                }
+
+                if (diffs <= allowedDiffs)
+                    hLines.Add(i);
+            }
+
+            for (var i = 0; i < mirror[0].Length - 1; i++)
+            {
+                var diffs = 0;
+                // check vertical
+                for (var j = 0; j < mirror.Length; j++)
+                {
+
+                    if (mirror[j][i] != mirror[j][i + 1])
+                    {
+                        diffs++;
+                    }
+                }
+
+                if (diffs <= allowedDiffs)
+                    vLines.Add(i);
+            }
+
+            return (hLines, vLines);
+        }
+
     }
 }
