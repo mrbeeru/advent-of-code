77435d19ab699d12b4e3f5e235707cabfa78c15c
mrbeeru <andrei_bublea@yahoo.com>
2023-12-15 21:16:30 +0200

    aoc 2023 day 15

---
+++ AdventOfCode.Test/Y2023/TestDay15.cs
@@ -0,0 +1,28 @@
+﻿using AdventOfCode.Quizzes.Y2023;
+
+namespace AdventOfCode.Test.Y2023
+{
+    public class TestDay15 : TestBase
+    {
+        [Fact]
+        public void Part1()
+        {
+            var day = new Day15(InputProvider);
+
+            Assert.Equal(1320, day.Part1());
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var day = new Day15(InputProvider);
+
+            Assert.Equal(145, day.Part2());
+        }
+
+        protected override string[] AocInput()
+        {
+            return ["rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"];
+        }
+    }
+}
---
+++ AdventOfCode/Quizzes/Y2023/Day15.cs
@@ -0,0 +1,61 @@
+﻿using AdventOfCode.Reader;
+using MoreLinq;
+
+namespace AdventOfCode.Quizzes.Y2023
+{
+    [Aoc(year: 2023, day: 15)]
+    public class Day15(IInputProvider inputProvider) : IPartOne<long>, IPartTwo<long>
+    {
+        private record Lens(string Label, string FocalLen);
+
+        public long Part1() => inputProvider.GetInput().Single().Split(',').Select(HASH).Sum();
+
+        public long Part2()
+        {
+            var lenses = inputProvider.GetInput().Single().Split(',').Select(x => x.Split(["-", "="], StringSplitOptions.None)).Select(x => new Lens(x[0], x[1]));
+            var boxes = new Dictionary<int, List<Lens>>();
+            Enumerable.Range(0, 256).ForEach(x => boxes.Add(x, []));
+
+            foreach (var lens in lenses)
+            {
+                var boxNumber = HASH(lens.Label);
+                var list = boxes[boxNumber];
+                var items = list.Where(x => x.Label == lens.Label);
+
+                if (lens.FocalLen == "")
+                {
+                    if (items.Any())
+                    {
+                        list.Remove(items.First());
+                    }
+                }
+                else if (items.Any())
+                {
+                    var index = list.IndexOf(items.First());
+                    list[index] = lens;
+                }
+                else
+                {
+                    list.Add(lens);
+                }
+            }
+
+            // total focal power
+            return boxes.Select(box => box.Value.Select((lenses, index) => (box.Key + 1) * (index + 1) * int.Parse(lenses.FocalLen)).Sum()).Sum();
+        }
+
+        static int HASH(string input)
+        {
+            var hash = 0;
+
+            foreach (var c in input)
+            {
+                hash += c;
+                hash *= 17;
+                hash %= 256;
+            }
+
+            return hash;
+        }
+    }
+}
