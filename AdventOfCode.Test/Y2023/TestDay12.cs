ad57a07f6caac781d4b395ec9b0cb7cc1a7238e5
mrbeeru <andrei_bublea@yahoo.com>
2023-12-23 22:55:44 +0200

    aoc 2023 day 12 part 1

---
+++ AdventOfCode.Test/Y2023/TestDay12.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCode.Quizzes.Y2023;
+
+namespace AdventOfCode.Test.Y2023
+{
+    public class TestDay12 : TestBase
+    {
+        [Fact]
+        public void Part1()
+        {
+            var day = new Day12(InputProvider);
+            Assert.Equal(405, day.Part1());
+        }
+
+        protected override string[] AocInput()
+        {
+            return [
+                "???.### 1,1,3",
+                ".??..??...?##. 1,1,3",
+                "?#?#?#?#?#?#?#? 1,3,1,6",
+                "????.#...#... 4,1,1",
+                "????.######..#####. 1,6,5",
+                "?###???????? 3,2,1",
+            ];
+        }
+    }
+}
---
+++ AdventOfCode/Quizzes/Y2023/Day12.cs
@@ -0,0 +1,73 @@
+﻿using AdventOfCode.Extensions;
+using AdventOfCode.Reader;
+
+namespace AdventOfCode.Quizzes.Y2023
+{
+    [Aoc(year: 2023, day: 12)]
+    public class Day12(IInputProvider inputProvider) : IPartOne<long>
+    {
+        private record Spring(char[] Layout, int[] Groups);
+
+        public long Part1()
+        {
+            var input = inputProvider.GetInput().Select(x => new Spring([.. x.Split(" ")[0]], x.Split(" ")[1].Nums().ToArray())).ToList();
+            var sum = 0;
+
+            input.ForEach(x => Recurse(x.Layout, 0, x.Groups, 0, ref sum));
+            return sum;
+        }
+
+        static void Recurse(char[] input, int index, int[] nums, int numIndex, ref int sum)
+        {
+            // skip spaces;
+            while (index < input.Length && input[index] == '.')
+                index++;
+
+            // finished
+            if (index >= input.Length)
+                return;
+
+            if (input[index] == '?')
+                Recurse(input, index + 1, nums, numIndex, ref sum);
+
+            var num = nums[numIndex];
+
+            for (int i = 0; i < num; i++)
+            {
+                // bad, outside
+                if (i + index >=  input.Length)
+                    return;
+
+                //bad, need to place #
+                if (input[i + index] == '.')
+                    return;
+            }
+
+            index += num;
+
+            if (numIndex == nums.Length - 1)
+            {
+                while (index < input.Length)
+                {
+                    //bad, need all to be . or ?
+                    if (input[index] == '#')
+                    {
+                        return;
+                    }
+                    index++;
+                }
+                sum++;
+                return;
+            }
+
+            if (index <  input.Length && input[index] == '#')
+            {
+                //bad, need to separate ###
+                return;
+            }
+
+            // pick next num
+            Recurse(input, index + 1, nums, numIndex + 1, ref sum);
+        }
+    }
+}
